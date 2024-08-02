2ecc841a65f243e2fe0740291498d1463809e22e
Chaithanya <itsmechaithanyam@gmail.com>
2024-08-02 23:11:47 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="h-screen w-screen App flex flex-col items-center justify-center bg-blue-950 text-white">
       <h1 className="text-6xl font-bold mb-3">Chessboard</h1>
-      <div className='w-full mb-8 pl-20 pr-20'>
+      <div className='w-full mb-8 pl-32 pr-32'>
         <h4 className='capitalize text-center'>The current chessboard implementation visually demonstrates the directions in which a bishop can move in      
         chess. When you click a cell, it highlights all the diagonally aligned cells that a bishop would be able to move to from that position.</h4>
       </div>
